51d2cd14df203637f0843690bea92e47d40b4bde
AndreiStan <andystan291@yahoo.com>
2020-04-25 18:23:44 +0300

    Second commit

---
+++ Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Grpc.Net.Client;
+using Tema2CNA.Protos;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Zodiac.ZodiacClient(channel);
+
+            var request = new DateRequest();
+            request.Birthday = ValidateInput();
+            var response = client.GetZodiacSign(request);
+            Console.WriteLine($"Zodia este {response.Sign}");
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static string ValidateInput()
+        {
+            Console.WriteLine("Enter your birthdate:");
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                DateTime dateTime;
+                if (!DateTime.TryParseExact(input, "MM/dd/yyyy", null, DateTimeStyles.None, out dateTime))
+                {
+                    Console.WriteLine("Invalid date input.");
+                    input = Console.ReadLine();
+                }
+                return input;
+            }
+        }
+    }
+}
---
+++ Tema2CNA/Services/ZodiacService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Tema2CNA.Protos;
+
+namespace Tema2CNA.Services
+{
+    public class ZodiacService : Zodiac.ZodiacBase
+    {
+        public override Task<ZodiacReply> GetZodiacSign(DateRequest request, ServerCallContext context)
+        {
+            var birthday = request.Birthday;
+            var year = GetYears(birthday);
+            var dateTime = DateTime.Parse(birthday);
+            var path = "intervale-zodii.txt";
+            var file = File.ReadAllLines(path);
+
+            string sign = GetSign(file, year, dateTime);
+
+            Console.WriteLine("Your sign is : " + sign);
+            return Task.FromResult(new ZodiacReply(){Sign = sign});
+        }
+
+
+        public static DateTime GetDate(string date, int year)
+        {
+            DateTime dateTime = DateTime.Parse(date);
+            var birthYears = dateTime.AddYears(-dateTime.Year + year);
+
+            return birthYears;
+        }
+
+        public static int GetYears(string date)
+        {
+            string[] pieces = date.Split('/');
+
+            return int.Parse(pieces[2]);
+        }
+
+        public static string GetSign(string[] files, int year, DateTime birthdate)
+        {
+            DateTime startDay;
+            DateTime endDay;
+            string sign;
+
+            for(int i = 0 ; i< files.Length; i+=3)
+            {
+                sign = files[i];
+                startDay = GetDate(files[i+1], year);
+                endDay = GetDate(files[i+2], year);
+                if (startDay <= birthdate && birthdate <= endDay)
+                {
+                    return sign;
+                }
+            }
+
+            return "Eroare";
+        }
+    }
+}
